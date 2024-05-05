f014b73c77de6500f506334293a8f9681a825d3a
NGUYỄN HUỲNH SƠN <142656687+lionstreet1235@users.noreply.github.com>
2024-05-05 19:08:00 +0700

    Khởi tạo project

---
+++ FiLTran/src/main/java/com/example/filtran/FiLTranApplication.java
@@ -0,0 +1,13 @@
+package com.example.filtran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiLTranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiLTranApplication.class, args);
+    }
+
+}
---
+++ FiLTran/src/test/java/com/example/filtran/FiLTranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filtran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiLTranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
